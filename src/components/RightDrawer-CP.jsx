7b5d6c64eff9edd62025263dc4117ac044c945ba
lijiaqiang <lijiaqiang@outlook.com>
2018-12-21 10:27:53 +0800

    add slide action for siteAddForm

--- src/components/RightDrawer-CP.jsx
+++ src/components/RightDrawer-CP.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Style from './style.css'
-import { Drawer, Button } from "@material-ui/core";
+import { Slide, Button, ClickAwayListener } from "@material-ui/core"
 
 const RightDrawer = (props) => {
   let siteAdrInput, siteNameInput
@@ -17,7 +17,7 @@ const RightDrawer = (props) => {
     handleCloseForm()
   }
 
-  const handleCloseForm = () => {
+  const handleClickAway = () => {
 
     wantCloseRightDrawer()
     siteAdrInput.value = ''
@@ -25,16 +25,46 @@ const RightDrawer = (props) => {
   }
 
   return (
-    <div >
-      <Drawer anchor="right" open={signal} onClose={handleCloseForm} variant="temporary">
-        <input id="site-adr" ref={input => siteAdrInput = input} />
-        <input id="site-name" ref={input => siteNameInput = input}/>
-        <Button variant="contained" color="primary"
-        onClick={handleSubmit}
-        >确认</Button>
-      </Drawer>
+    <div>        
+      <Slide direction="left" in={signal} mountOnEnter unmountOnExit>
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <div className={Style.sideDrawer}></div>
+        </ClickAwayListener>     
+      </Slide>
     </div>
   )
+}
+
+{
+  // <div className={Style.siteForm}>
+  //         <label>
+  //           <div className={Style.siteIcon}></div>
+  //         </label>
+  //         <label>
+  //           <span>标题</span>
+  //           <div>
+  //             <input type="text" className={Style.inputField} 
+  //                 ref={input => siteNameInput = input}/>
+  //           </div>
+  //         </label>
+  //         <label>
+  //           <span>网址</span>
+  //           <div>
+  //             <input type="text" className={Style.inputField} 
+  //                 ref={input => siteAdrInput = input} />
+  //           </div>
+  //         </label>
+  //         <button></button>
+  //         <label>
+  //           <span></span>
+  //           <div>
+  //             <input type="text" className={Style.inputField}/>
+  //           </div>
+  //         </label>
+  //         <Button variant="contained" color="primary"
+  //             onClick={handleSubmit} className={Style.testButton}
+  //             >确认</Button>
+  //       </div>   
 }
 
 export default RightDrawer
